1f133267cf8a0852e95289cca89a2d69ca0dd78e
Maximiliam999 <maxipopstar@hotmail.com>
2024-03-21 14:46:15 +0000

    added post_detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path 
 
 urlpatterns = [
-    path('', views.Postlist.as_view(), name='home')
+    path('', views.Postlist.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
