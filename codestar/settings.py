0cdb040196990e93cda1ad858440417bdf0bd607
Maximiliam999 <maximanuel777@gmail.com>
2024-04-08 14:49:47 +0000

    Change from codeanywhere

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-maximiliam99-djangoblog-5atc5ttt32p.ws-eu110.gitpod.io', 
+ALLOWED_HOSTS = ['8000-maximiliam99-djangoblog-n4h3j7l19dg.ws-eu110.gitpod.io', 
                 '.herokuapp.com']
 
 
